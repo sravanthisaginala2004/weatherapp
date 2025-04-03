6ef5973046eb1e9e35df948517e41dbd1b235b45
sravanthisaginala2004 <2200080129@kluniversity.in>
2025-04-03 22:20:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def index():
     return render_template('index.html', weather_data=weather_data, error=error)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
